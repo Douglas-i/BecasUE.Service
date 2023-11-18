b1d9dcfc97592ebd9428e3f366e9abee877e4391
Douglas-i <hdouglasu@gmai.com>
2023-11-17 21:54:53 -0600

    Relacion ProgramasTitulacion Especialidad

---
+++ DataAccess/Entities/ProgramasTitulacion.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.Entities.Relaciones;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class ProgramasTitulacion
+    {
+        [Key]
+        [Required]
+        public int ProgramaId { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100)]
+        public string TituloPrograma { get; set; }
+
+        [Required]
+        public int CantidadCredito { get; set; }
+
+        public int NumeroDiplomados { get; set; }
+
+        [StringLength(maximumLength: 100)]
+        public string Duracion { get; set; }
+
+        [Required]
+        public int EspecialidadId { get; set; } // Clave foránea
+        public TiposEspecialidad Especialidad { get; set; } // Propiedad de navegación
+    }
+}
--- DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
+++ DataAccess/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -99,6 +99,40 @@ namespace DataAccess.Migrations
                     b.ToTable("ExperienciaLaboral");
                 });
 
+            modelBuilder.Entity("DataAccess.Entities.ProgramasTitulacion", b =>
+                {
+                    b.Property<int>("ProgramaId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("ProgramaId"));
+
+                    b.Property<int>("CantidadCredito")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Duracion")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
+
+                    b.Property<int>("EspecialidadId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("NumeroDiplomados")
+                        .HasColumnType("int");
+
+                    b.Property<string>("TituloPrograma")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
+
+                    b.HasKey("ProgramaId");
+
+                    b.HasIndex("EspecialidadId");
+
+                    b.ToTable("ProgramasTitulacion");
+                });
+
             modelBuilder.Entity("DataAccess.Entities.SolicitudesAceptadas", b =>
                 {
                     b.Property<int>("SolicitudAceptadaId")
@@ -173,6 +207,22 @@ namespace DataAccess.Migrations
                     b.HasKey("universidadId");
 
                     b.ToTable("Universidad");
+                });
+
+            modelBuilder.Entity("DataAccess.Entities.ProgramasTitulacion", b =>
+                {
+                    b.HasOne("DataAccess.Entities.TiposEspecialidad", "Especialidad")
+                        .WithMany("ProgramasTitulaciones")
+                        .HasForeignKey("EspecialidadId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Especialidad");
+                });
+
+            modelBuilder.Entity("DataAccess.Entities.TiposEspecialidad", b =>
+                {
+                    b.Navigation("ProgramasTitulaciones");
                 });
 #pragma warning restore 612, 618
         }
